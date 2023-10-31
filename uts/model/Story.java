47d80166db88f3ff327adfb082a144e1b7702751
aqbil D.N <117892781+Aqbil-DN@users.noreply.github.com>
2023-10-31 11:38:45 +0700

    Update Story.java

--- uts/model/Story.java
+++ uts/model/Story.java
@@ -2,6 +2,7 @@ package uts.model;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Story extends Post {
     private int duration;
@@ -26,23 +27,25 @@ public class Story extends Post {
     }
 
     public void showPost(String id, User user) {
-        for (Post p : user.getList_post()) {
-            if (p instanceof Story) {
-                Story story = (Story) p;
-                if (story.getPostId() == id) {
-                    System.out.println("Post ID: " + story.getPostId() +
-                            ", Status: " + story.getStatus() +
-                            ", Type: " + story.getPostType() +
-                            ", Time Upload: " + story.getTimeUpload() +
-                            ", Duration: " + story.getDuration() +
-                            ", Views: " + story.getView());
-                    break;
-                }
+    List<Post> listPost = user.getList_post();
+    for (int i = 0; i < listPost.size(); i++) {
+        Post p = listPost.get(i);
+        if (p instanceof Story) {
+            Story story = (Story) p;
+            if (story.getPostId().equals(id)) {
+                System.out.println("Post ID: " + story.getPostId() +
+                        ", Status: " + story.getStatus() +
+                        ", Type: " + story.getPostType() +
+                        ", Time Upload: " + story.getTimeUpload() +
+                        ", Duration: " + story.getDuration() +
+                        ", Views: " + story.getView());
+                return; // Exit the loop when the post is found
             }
         }
-        System.out.println("Post not found");
-
     }
+    System.out.println("Post not found");
+}
+
     public Story(String postId, ContentState status, Timestamp timeUpload, ContentType postType,
                  ArrayList<Comment> comments, int duration, int view) {
         super(postId, status, timeUpload, postType, comments);
