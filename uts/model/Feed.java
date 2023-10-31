e5f50f84f07f427930ac6335c6dd3d70896c37bc
aqbil D.N <117892781+Aqbil-DN@users.noreply.github.com>
2023-10-31 11:33:36 +0700

    Update Feed.java

--- uts/model/Feed.java
+++ uts/model/Feed.java
@@ -25,23 +25,26 @@ public class Feed extends Post{
         this.likes = likes;
     }
 
-    public void showPost(String id, User user){
-        for (Post p : user.getList_post()) {
-            if(p instanceof Feed){
-                Feed feed = (Feed) p;
-                if(feed.getPostId()==id){
-                    System.out.println("Post ID: " + feed.getPostId() +
-                            ", Status: " + feed.getStatus() +
-                            ", Type: " + feed.getPostType() +
-                            ", Time Upload: " + feed.getTimeUpload() +
-                            ", Caption: " + feed.getCaption() +
-                            ", Likes: " + feed.getLikes());
-                    break;
-                }
+    public void showPost(String id, User user) {
+    List<Post> listPost = user.getList_post();
+    for (int i = 0; i < listPost.size(); i++) {
+        Post p = listPost.get(i);
+        if (p instanceof Feed) {
+            Feed feed = (Feed) p;
+            if (feed.getPostId().equals(id)) {
+                System.out.println("Post ID: " + feed.getPostId() +
+                        ", Status: " + feed.getStatus() +
+                        ", Type: " + feed.getPostType() +
+                        ", Time Upload: " + feed.getTimeUpload() +
+                        ", Caption: " + feed.getCaption() +
+                        ", Likes: " + feed.getLikes());
+                return; // Exit the loop when the post is found
             }
         }
-        System.out.println("tidak ditemukan");
     }
+    System.out.println("tidak ditemukan");
+}
+
     public Feed(String postId, ContentState status, Timestamp timeUpload, ContentType postType,
                 ArrayList<Comment> comments, String caption, int likes) {
         super(postId, status, timeUpload, postType, comments);
